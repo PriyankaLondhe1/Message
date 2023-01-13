55818ba28fb11c5ea08f46238b1e547ebe654021
PriyankaLondhe1 <priyankalondhe190@gmail.com>
2023-01-13 15:54:30 -0800

    second statement is added...

--- src/com/velocity/MessageDisplay.java
+++ src/com/velocity/MessageDisplay.java
@@ -3,6 +3,7 @@ package com.velocity;
 public class MessageDisplay {
 	public static void main(String[] args) {
 		System.out.println("hiiii...This is priyanka");
+		System.out.println("hlo from velocity");
 	}
 
 }
